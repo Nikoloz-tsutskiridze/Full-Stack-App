cc27bbc235cb3969bc8b48fa0698108c0b4c18d9
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2025-04-25 21:31:55 +0400

    Setup

---
+++ client/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import mkcert from "vite-plugin-mkcert";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [react(), mkcert(), tailwindcss()],
+});
