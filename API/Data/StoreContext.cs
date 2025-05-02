62d133392a97a9831269246ed0301d0b047d2703
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2025-05-02 23:45:27 +0400

    Setup

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -4,11 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class StoreContext : DbContext
+public class StoreContext(DbContextOptions<StoreContext> options) : DbContext(options)
 {
-  public StoreContext(DbContextOptions<StoreContext> options) : base(options)
-  {
-  }
-
   public DbSet<Product> Products { get; set; }
+  public required DbSet<Basket> Baskets { get; set; }
 }
